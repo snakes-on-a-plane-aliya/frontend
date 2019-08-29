92d1485eda746bce2d761c35222a160e48ec8c02
arsummers <aliyasummers1@gmail.com>
2019-08-28 19:12:43 -0700

    styled fonts, colors, buttons

--- src/Plane.js
+++ src/Plane.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import './Plane.css'
+import './styles/_vars.scss'
+import './styles/reset.scss'
+import './styles/_base.scss'
+import './styles/_layout.scss'
+import './styles/_plane.scss'
 import superagent from 'superagent'
 
 let api_url = 'https://mediator-git-blocker-awareness.snakes-on-a-plane.now.sh/api'
@@ -8,202 +12,6 @@ export default class Plane extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            // player : {
-            //     name : '',
-            //     x_pos : 0,
-            //     y_pos : 0,
-            // },
-            // rows : [[
-            //     {
-            //         cell_type : 'entrance',
-            //         x_pos : 0,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 1,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 2,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 3,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 4,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 5,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 6,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 7,
-            //         y_pos : 0,
-            //     },
-            //     {
-            //         cell_type : 'wall',
-            //         x_pos : 8,
-            //         y_pos : 0,
-            //     },
-                
-            // ],
-            // [
-            //     {
-            //         cell_type : 'wall',
-            //         x_pos : 0,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 1,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 2,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 3,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 4,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 5,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 6,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 7,
-            //         y_pos : 1,
-            //     },
-            //     {
-            //         cell_type : 'wall',
-            //         x_pos : 8,
-            //         y_pos : 1,
-            //     },
-            // ],
-            // [
-            //     {
-            //         cell_type : 'wall',
-            //         x_pos : 0,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 1,
-            //         y_pos : 2,
-                    
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 2,
-            //         y_pos : 2,
-                    
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 3,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 4,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 5,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 6,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 7,
-            //         y_pos : 2,
-            //     },
-            //     {
-            //         cell_type : 'wall',
-            //         x_pos : 8,
-            //         y_pos : 2,
-            //     },
-            // ],
-            // [
-            //     {
-            //         cell_type : 'exit',
-            //         x_pos : 0,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 1,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 2,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 3,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'aisle',
-            //         x_pos : 4,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 5,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 6,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'seat',
-            //         x_pos : 7,
-            //         y_pos : 3,
-            //     },
-            //     {
-            //         cell_type : 'exit',
-            //         x_pos : 8,
-            //         y_pos : 3,
-            //     },
-            // ]]
             grid: [],
             player_pos: {}
         }
@@ -250,36 +58,15 @@ export default class Plane extends React.Component {
                     {this.getRows()}
                 </div>
                 <div>
-                <button onClick={() => this.move("left")}>Left</button>
+                <button onClick={() => this.move("left")}>Move Left</button>
+                <button onClick={() => this.move("right")}>Move Right</button>
+                <button onClick={() => this.move("up")}>Move Up</button>
+                <button onClick={() => this.move("down")}>Move Down</button>
+
             </div>
             </div>
             
         )
-    }
-
-    // renderRows(){
-    //     const rowsToRender = []
-    //     for (let row of this.state.rows){ 
-    //         let cellsToRender = row.map(cell => {
-    //             let className = 'Cell ' + cell.cell_type
-
-    //             if (cell.x_pos === this.state.player.x_pos &&
-    //                 cell.y_pos === this.state.player.y_pos){
-    //                     className += ' player'
-    //                 }
-    //             return <div className={className}>{cell.cell_type}</div>
-    //         })
-    //         rowsToRender.push(cellsToRender)
-    //     }
-    //     return rowsToRender;
-    // }
-    //     render() {
-    //         return (<div className='Plane'>
-    //             {this.renderRows()}
-    //         </div>)
-    //     }
-
-
-    
+    } 
     
 }
